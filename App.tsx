7ac4999cbb71361a932e1572a4096ff0c5ddb7bd
designforesterforever-star <designforesterforever@gmail.com>
2025-11-22 14:58:18 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useEffect, useState } from 'react';
 import { ArrowDown, Linkedin, Dribbble, Instagram, Globe, Settings, Github, Twitter } from 'lucide-react';
 import Header from './components/Header';
